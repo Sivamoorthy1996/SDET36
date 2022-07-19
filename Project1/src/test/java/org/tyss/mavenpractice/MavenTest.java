9241ff3e5477261c8fa26ca87aa8ef4eec8033ea
HP <HP@LAPTOP-DR1P4N5S>
2022-07-19 11:44:56 +0530

    good morning

---
+++ Project1/src/test/java/org/tyss/mavenpractice/MavenTest.java
@@ -0,0 +1,19 @@
+package org.tyss.mavenpractice;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+@Test
+public void practice() {
+	System.out.println("test");
+}
+@Test
+public void practice1() {
+	System.out.println("test");
+}
+@Test
+public void practice2() {
+	System.out.println("test");
+}
+}
