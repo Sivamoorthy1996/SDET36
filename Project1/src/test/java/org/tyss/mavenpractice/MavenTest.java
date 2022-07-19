b76100437e9e1569628d6c6fbf8a8e3e468947ed
HP <HP@LAPTOP-DR1P4N5S>
2022-07-19 12:13:39 +0530

    third commit

--- Project1/src/test/java/org/tyss/mavenpractice/MavenTest.java
+++ Project1/src/test/java/org/tyss/mavenpractice/MavenTest.java
@@ -6,14 +6,17 @@ public class MavenTest {
 
 @Test
 public void practice() {
-	System.out.println("test");
+	System.out.println("testing");
 }
 @Test
 public void practice1() {
 	System.out.println("test");
-}
+}/**
+
 @Test
 public void practice2() {
-	System.out.println("test");
+	System.out.println("test all");
 }
+**/
+
 }
